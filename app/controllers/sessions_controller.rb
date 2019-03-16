b6fdc2483e15502e2730b018579bc050f1a8f02d
Aiko <ljpoyopoyo@gmail.com>
2019-03-16 15:42:00 +0900

    tet

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,25 +13,17 @@ class SessionsController < ApplicationController
     end
   end
 
-  private
-  def log_in(user)
-   session[:user_id] = user.id
-  end
-
-  render :new
-  end
-
   def destroy
     log_out
     redirect_to root_url, info: 'ログアウトしました'
   end
-
   private
   def log_in(user)
-    session[:user_id] = user.id
+   session[:user_id] = user.id
   end
 
   def log_out
     session.delete(:user_id)
     @current_user = nil
   end
+end
