51773c72f95c58adf4fe6a8eaf5a7ce70ab11c28
Aiko <ljpoyopoyo@gmail.com>
2019-04-18 19:23:08 +0900

    commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,10 +12,8 @@ class UsersController < ApplicationController
        render :new
       end
   end
-
-
   private
-    def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation)
-    end
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
